81c01d6149178baa8f756789cd49a068ca9dc58c
rakeshsamanta <32979313+rakeshsamanta@users.noreply.github.com>
2021-06-19 11:37:58 +0530

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,28 @@
+#print("hello")
+print("helloworld")
+#msg="rakesh"
+print("msg")
+msg="rakesh"
+print("msg")
+msg="d"
+a=5
+b=5
+print(a,"+",b)
+c=a+b
+print("c")
+print("c=", c)
+txt = "The best things in life are free!"
+if "free" in txt:
+    print("Yes, 'free' is present.")
+price = 49
+ts = "The price is ",price," dollars"
+print(ts)
+if a > b:
+    print("a more than b")
+else:
+    print("a less than or equal b")
+c = a+b
+print("c=", c)
+
+username=input("Enter username:")
+print("username is:"+username)
---
+++ hwmwrk.py
@@ -0,0 +1,8 @@
+name = raw_input("Enter your name:")
+age = raw_input("Enter your age:")
+education = raw_input("Enter your education:")
+hobby = raw_input("Enter your hobby:")
+print("NAME : " + name)
+print("AGE : " + age)
+print("EDUCATION : " + education)
+print("HOBBY" + hobby)
