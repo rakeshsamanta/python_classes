cfc581a0cda86c6c05fc9ed4484bc0f236769dd1
rakeshsamanta <32979313+rakeshsamanta@users.noreply.github.com>
2021-06-20 20:55:48 +0530

    Add files via upload

---
+++ temperature.py
@@ -0,0 +1,4 @@
+#farenheit to celcius
+farenheit = float(input("Enter the farenheit temperature :"))
+celcius = farenheit - 32 * 0.5556
+print("%0.02f farenheit is equal to %0.02f celcius" %(farenheit,celcius))
